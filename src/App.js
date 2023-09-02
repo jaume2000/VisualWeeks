2e7951d5e8b5094ad2ee1915916ded3c64d28839
jaume2000 <jaumeig95@gmail.com>
2023-09-03 01:27:59 +0200

    finished functional time

--- src/App.js
+++ src/App.js
@@ -5,14 +5,16 @@ function App() {
 
 
   let years = 80;
-  let weeks = 80 * 52;
+  let lived = 21.9;
+  let weeks = Math.floor(years * 52.14);
+  let weeks_lived = Math.floor(lived * 52.14);
 
 
   return (
     <div className="App">
       <h1>{"¿Cuanto tiempo te queda?"}</h1>
-      <div>
-        
+      <div className="week_container">
+        {Array.from({length: weeks}, (_, i) => <div key={i} className={"week" + (i <= weeks_lived ? ' lived' : '') + (i % 520 == 0 ? ' year' : '')}/>)}
       </div>
     </div>
   );
