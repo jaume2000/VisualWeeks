3a7d0f4a38daf1a49d5e4384065c656c849413d3
jaume2000 <jaumeig95@gmail.com>
2023-09-03 00:51:23 +0200

    initialization of project

---
+++ src/App.js
@@ -0,0 +1,21 @@
+
+import './App.css';
+
+function App() {
+
+
+  let years = 80;
+  let weeks = 80 * 52;
+
+
+  return (
+    <div className="App">
+      <h1>{"¿Cuanto tiempo te queda?"}</h1>
+      <div>
+        
+      </div>
+    </div>
+  );
+}
+
+export default App;
